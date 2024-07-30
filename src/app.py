e8b895aa3841849217d9042c728d5b0e22e81e0d
amalia1501 <amibottini@hotmail.com>
2024-07-30 19:14:16 +0000

    html

--- src/app.py
+++ src/app.py
@@ -33,10 +33,49 @@
 #     pred_class = class_dict[prediction]
 #     st.write("Prediction:", pred_class)
 
-from flask import Flask
+from flask import Flask, request, render_template
+from pickle import load
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+# Obtengo el modelo
+model = load(open("/workspace/Tutorial-de-aplicacion-web-de-ml-usando-flask/src/decision_tree_classifier_default_42.sav", "rb"))
+
+# Diccionario con la prediccion 
+class_dict = {
+    "0": "No Survived",
+    "1": "Survived"
+}
+
+
+@app.route("/", methods= ["GET", "POST"])
+
+
+def index():
+    if request.method == "POST":
+        
+        # Obtain values from form
+        pclass = float(request.form["pclass"])
+        sex = float(request.form["sex"])
+        FamMembers = float(request.form["FamMembers"])
+        fare = float(request.form["fare"])
+        embarked = float(request.form["embarked"])
+
+        # pclass = st.selectbox("Passenger Class", options=[1, 2, 3])
+# sex = st.selectbox("Sex of the Passenger", options=["Male", "Female"])
+# FamMembers = st.slider("Number of Siblings/Spouses Aboard", min_value=0, max_value=10, step=1)
+# fare = st.slider("Fare Paid", min_value=0.0, max_value=500.0, step=0.1)
+# embarked = st.selectbox("Place of Embarkment", options=["C", "Q", "S"])
+
+# # Convertir variables categóricas a valores numéricos
+# sex_n = 1 if sex == "Male" else 0
+# embarked_n = {"C": 0, "Q": 1, "S": 2}[embarked]
+        
+        data = [[pclass, sex, FamMembers, fare, embarked]]
+        prediction = str(model.predict(data)[0])
+        pred_class = class_dict[prediction]
+        
+    else:
+        pred_class = None
     
+    return render_template("index.html", prediction = pred_class)
